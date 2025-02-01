e61b5bf96eb118d79fce1f74a19aa973d7d81bda
Florian Culié <florian.culie@bassetti.fr>
2025-02-01 16:41:07 +0100

    small tweaks over date digit numbers in URL.

--- src/plugins/french_newspaper/french_newspaper.py
+++ src/plugins/french_newspaper/french_newspaper.py
@@ -8,17 +8,17 @@ import logging
 logger = logging.getLogger(__name__)
 
 #                                          YYYY/MM/DD/fr/newspapername
-KIOSKO_IMAGE_URL = "https://img.kiosko.net/{}/{}/{}/fr/{}.750.jpg"
+KIOSKO_IMAGE_URL = "https://img.kiosko.net/{}}/fr/{}.750.jpg"
 class FrenchNewspaper(BasePlugin):
     def generate_image(self, srttings, device_config):
         newspaper_slug = "l_equip"
 
         today = datetime.today()
 
-        image_url = KIOSKO_IMAGE_URL.format(today.year, today.month, today.day, newspaper_slug)
+        image_url = KIOSKO_IMAGE_URL.format(today.strftime('%Y/%m/%d'), newspaper_slug)
         image = get_image(image_url)
         if image:
-            logging.info(f"Found {newspaper_slug} from covert for {today.strftimer('%Y-%m-%d')}")
+            logging.info(f"Found {newspaper_slug} from covert for {today.strftime('%Y-%m-%d')}")
             # expand height if newspaper is wider than resolution
             img_width, img_height = image.size
             desired_width, desired_height = device_config.get_resolution()
